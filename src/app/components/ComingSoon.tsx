485a81681c7e7d1e59bbe9eb61d2597e356ddcff
gathumartins <gathumartins@gmail.com>
2024-09-16 16:54:53 +0300

    started building coming soon page

---
+++ src/app/components/ComingSoon.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function ComingSoon() {
+  return (
+    <div className="">
+        <section className="bg-[#1D4C948C] opacity-[55]">
+            <div className="container h-lvh flex justify-between items-end py-[60px] gap-5">
+                <div>Form</div>
+            <div>CopyRight</div>
+            </div>
+        </section>
+    </div>
+  )
+}
+
+export default ComingSoon
