fa0cf2bfb5671e3bf8e545253ad1199a22f0ab5d
gathumartins <gathumartins@gmail.com>
2024-09-18 10:35:34 +0300

    working on coming soon page

--- src/app/components/ComingSoon.tsx
+++ src/app/components/ComingSoon.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 function ComingSoon() {
   return (
-    <div className="">
-        <section className="bg-[#1D4C948C] opacity-[55]">
+    <div className="bg-[url('/images/hero.webp')] bg-cover bg-center bg-no-repeat">
+        <section className="bg-[#1D4C94] opacity-55">
             <div className="container h-lvh flex justify-between items-end py-[60px] gap-5">
                 <div>Form</div>
-            <div>CopyRight</div>
+                <div>CopyRight</div>
             </div>
         </section>
     </div>
