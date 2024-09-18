24977f9bccafd392cafa99d1f78b6d6e20258bea
gathumartins <gathumartins@gmail.com>
2024-09-18 22:52:27 +0300

    updating the form bg

--- src/app/components/ComingSoon.tsx
+++ src/app/components/ComingSoon.tsx
@@ -3,10 +3,12 @@ import React from 'react'
 function ComingSoon() {
   return (
     <div className="bg-[url('/images/hero.webp')] bg-cover bg-center bg-no-repeat">
-        <section className="bg-[#1D4C94] opacity-55">
+        <section className="bg-[#1D4C94]/55">
             <div className="container h-lvh flex justify-between items-end py-[60px] gap-5">
-                <form>Form</form>
-                <div>CopyRight</div>
+                <form name="contact" className="bg-[#B39B47]/90 backdrop-blur-sm w-[700px] min-h-[325px] rounded-xl">
+                    
+                </form>
+                <div>Copyright {(new Date().getFullYear())} @ Imara Fellowship. All rights reserved.</div>
             </div>
         </section>
     </div>
