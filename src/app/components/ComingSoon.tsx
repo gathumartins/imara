bb519be373020021486dfc372e6ab843854c9086
gathumartins <gathumartins@gmail.com>
2024-09-18 10:58:46 +0300

    adding tailwind config file and public folder

--- src/app/components/ComingSoon.tsx
+++ src/app/components/ComingSoon.tsx
@@ -5,7 +5,7 @@ function ComingSoon() {
     <div className="bg-[url('/images/hero.webp')] bg-cover bg-center bg-no-repeat">
         <section className="bg-[#1D4C94] opacity-55">
             <div className="container h-lvh flex justify-between items-end py-[60px] gap-5">
-                <div>Form</div>
+                <form>Form</form>
                 <div>CopyRight</div>
             </div>
         </section>
