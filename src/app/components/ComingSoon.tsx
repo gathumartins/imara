ba5f3eff9ee01bf8772d85164ffd50b85b85b858
gathumartins <gathumartins@gmail.com>
2024-09-19 19:56:42 +0300

    updated subscribe form errors

--- src/app/components/ComingSoon.tsx
+++ src/app/components/ComingSoon.tsx
@@ -6,7 +6,7 @@ import axios from 'axios';
 function ComingSoon() {
     const [email, setEmail] = useState("");
   const [state, setState] = useState("idle");
-  const [errorMsg, setErrorMsg] = useState(null);
+  const [errorMsg, setErrorMsg] = useState<{ title: string} | null>(null);
 
   const subscribe = async (e:any) => {
     e.preventDefault();
@@ -37,10 +37,22 @@ function ComingSoon() {
                 <div className="bg-[#B39B47]/90 backdrop-blur-sm w-full md:w-[650px] rounded-xl px-[60px] py-[40px]">
                     <h1 className="font-light text-2xl sm:text-3xl mb-2">Our New Website</h1>
                     <h2 className="font-extrabold text-white text-4xl sm:text-6xl mb-8">Coming Soon</h2>
-                    <form action="" className="flex gap-4 w-full sm:w-[380px]">
+                    <form action="">
+                    <section className="flex gap-4 w-full sm:w-[380px]">
                       <label htmlFor="email" className="sr-only">Email</label>
                       <input type="email" name="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Enter your e-mail address" className="rounded-lg py-2 px-6 flex-grow"/>
                       <input type="submit" value="Notify Me" className="bg-[#1D4C94] rounded-lg text-center p-3 cursor-pointer" disabled={state === "Loading"} onClick={subscribe}/>
+                    </section>
+                    <section className='text-left h-4'>
+                      {state === "Error" && errorMsg && (
+                        <div className="text-red-500 text-md">*{errorMsg.title}</div>
+                            )}
+                            {state === "Success" && (
+                              <div className="text-green-500 text-md">
+                                Success. Thank You Subscribing our Newsletter
+                              </div>
+                        )}
+                      </section>
                     </form>
                 </div>
                 <div className="text-center items-center md:items-end">&copy; {(new Date().getFullYear())} Imara Fellowship. All rights reserved.</div>
