b1d3dbf099e009412ba23fa9ac2a05cf2c4ce1dd
gathumartins <gathumartins@gmail.com>
2024-09-19 07:17:46 +0300

    updating the newsletter form

--- src/app/components/ComingSoon.tsx
+++ src/app/components/ComingSoon.tsx
@@ -4,11 +4,17 @@ function ComingSoon() {
   return (
     <div className="bg-[url('/images/hero.webp')] bg-cover bg-center bg-no-repeat">
         <section className="bg-[#1D4C94]/55">
-            <div className="container h-lvh flex justify-between items-end py-[60px] gap-5">
-                <form name="contact" className="bg-[#B39B47]/90 backdrop-blur-sm w-[700px] min-h-[325px] rounded-xl">
-                    
-                </form>
-                <div>Copyright {(new Date().getFullYear())} @ Imara Fellowship. All rights reserved.</div>
+            <div className="container h-lvh flex flex-col md:flex-row justify-end md:justify-between md:items-end py-[60px] gap-5">
+                <div className="bg-[#B39B47]/90 backdrop-blur-sm w-full md:w-[650px] rounded-xl px-[60px] py-[40px]">
+                    <h1 className="font-light text-3xl mb-2">Our New Website</h1>
+                    <h2 className="font-extrabold text-white text-6xl mb-8">Coming Soon</h2>
+                    <form action="" className="flex gap-4">
+                      <label htmlFor="email" className="sr-only">Email</label>
+                      <input type="email" name="email" id="email"  placeholder="Enter your e-mail address" className="rounded-lg py-2 px-6"/>
+                      <input type="submit" value="Notify Me" className="bg-[#1D4C94] rounded-lg text-center p-3"/>
+                    </form>
+                </div>
+                <div className="text-center items-center md:items-end">Copyright {(new Date().getFullYear())} @ Imara Fellowship. All rights reserved.</div>
             </div>
         </section>
     </div>
