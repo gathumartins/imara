e4191897cec1f307ef242a83d5013e1b84eb5457
gathumartins <gathumartins@gmail.com>
2024-11-18 07:58:42 +0300

    updating the main commit

--- src/app/(user)/about/page.tsx
+++ src/app/(user)/about/page.tsx
@@ -1,10 +1,14 @@
+import AboutUs from '@/app/components/AboutUs'
 import MiniBanner from '@/app/components/MiniBanner'
+import Program from '@/app/components/Program'
 import React from 'react'
 
 function page() {
   return (
     <>
     <MiniBanner/>
+    <AboutUs/>
+    <Program/>
     </>
   )
 }
---
+++ src/app/components/AboutUs.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function AboutUs() {
+  return (
+    <article className="bg-white">
+        <section className="container py-12">
+            
+        </section>
+    </article>
+  )
+}
+
+export default AboutUs
