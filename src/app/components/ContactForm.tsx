7200df14dd4f0b5caf8be7bf7b6bcd8b1868d3b2
gathumartins <gathumartins@gmail.com>
2024-11-22 14:00:50 +0300

    coded the contact form ui

--- src/app/components/ContactForm.tsx
+++ src/app/components/ContactForm.tsx
@@ -28,7 +28,7 @@ function ContactForm() {
             </h5>
           </header>
           <div className="flex flex-col md:flex-row justify-between gap-5">
-            <div className="bg-iSecondary rounded p-4 py-8 flex h-full place-items-center w-full md:w-1/3 lg:w-1/3 ">
+            <div className="bg-iSecondary rounded p-4 py-8 flex place-items-center w-full md:w-1/2 lg:w-1/3 ">
               <div className="w-[263px] mx-auto flex flex-col gap-5">
                 <div className="flex flex-row gap-5 place-items-center">
                   <div className="w-12 h-12 rounded border-1 border-iBlue p-3 flex justify-center place-items-center text-white">
@@ -82,7 +82,94 @@ function ContactForm() {
                 </div>
               </div>
             </div>
-            <div className="w-full md:w-2/3 lg:w-1/3"></div>
+            <div className="w-full md:w-1/2 lg:w-2/3">
+              <form
+                action=""
+                className="grid grid-cols-1 lg:grid-cols-2 gap-x-5 gap-y-3"
+              >
+                <div className="flex flex-col gap-1">
+                  <label
+                    htmlFor="fullName"
+                    className="font-avenirRoman text-base text-iNeutral"
+                  >
+                    Full Name
+                  </label>
+                  <input
+                    type="text"
+                    name="fullName"
+                    id="fullName"
+                    placeholder="Enter Full Name"
+                    className="p-2 rounded border border-white shadow focus:outline-none focus:border-iBlue focus:ring-iBlue focus:right-1 placeholder:text-iNeutral placeholder:text-sm" required
+                  />
+                </div>
+                <div className="flex flex-col gap-1">
+                  <label
+                    htmlFor="email"
+                    className="font-avenirRoman text-base text-iNeutral"
+                  >
+                    Email Address
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    placeholder="Enter Email Address"
+                    className="p-2 rounded border border-white shadow focus:outline-none focus:border-iBlue focus:ring-iBlue focus:right-1 placeholder:text-iNeutral placeholder:text-sm" required
+                  />
+                </div>
+                <div className="flex flex-col gap-1">
+                  <label
+                    htmlFor="phone"
+                    className="font-avenirRoman text-base text-iNeutral"
+                  >
+                    Phone Number
+                  </label>
+                  <input
+                    type="tel"
+                    name="phone"
+                    id="phone"
+                    placeholder="Enter Phone Number"
+                    className="p-2 rounded border border-white shadow focus:outline-none focus:border-iBlue focus:ring-iBlue focus:right-1 placeholder:text-iNeutral placeholder:text-sm" required
+                  />
+                </div>
+                <div className="flex flex-col gap-1">
+                  <label
+                    htmlFor="subject"
+                    className="font-avenirRoman text-base text-iNeutral"
+                  >
+                    Subject
+                  </label>
+                  <input
+                    type="text"
+                    name="subject"
+                    id="subject"
+                    placeholder="Enter Subject"
+                    className="p-2 rounded border border-white shadow focus:outline-none focus:border-iBlue focus:ring-iBlue focus:right-1 placeholder:text-iNeutral placeholder:text-sm" required
+                  />
+                </div>
+                <div className="flex flex-col gap-1 lg:col-span-2">
+                  <label
+                    htmlFor="message"
+                    className="font-avenirRoman text-base text-iNeutral"
+                  >
+                    Message
+                  </label>
+                  <textarea
+                    name="message"
+                    id="message"
+                    rows={2}
+                    placeholder="Enter Message"
+                    className="p-2 rounded border border-white shadow focus:outline-none focus:border-iBlue focus:ring-iBlue focus:right-1 placeholder:text-iNeutral placeholder:text-sm" required
+                  ></textarea>
+                </div>
+                <button
+                  type="submit"
+                  className="bg-iBlue hover:bg-iSecondary px-3 text-white w-[180px] mx-auto h-[50px] flex place-items-center justify-center text-center rounded text-xl font-avenirRoman ml-0 mt-4"
+                >
+                  Send Message
+                </button>
+              </form>
+            </div>
           </div>
         </section>
       </section>
