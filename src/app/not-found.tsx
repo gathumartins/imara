ed06aa67b2e456c3c3114046a0e61e24b31c8190
gathumartins <gathumartins@gmail.com>
2024-11-22 12:10:07 +0300

    adjusted the 404 font size in samll devices

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -9,13 +9,13 @@ function NotFound() {
     >
       <section className="container h-full py-12 px-4 flex justify-center place-items-center md:justify-end md:place-items-end">
         <div className="max-w-[600px] md:w-[600px] flex flex-col">
-          <h1 className="text-iBlue font-avenirBlack text-9xl text-center mb-10">
+          <h1 className="text-iBlue font-avenirBlack text-5xl sm:text-9xl text-center mb-10">
             404
           </h1>
-          <h2 className="text-black font-avenirNextRegular text-6xl text-center">
+          <h2 className="text-black font-avenirNextRegular text-3xl  sm:text-6xl text-center">
             Page Not Found
           </h2>
-          <p className="max-w-[600px] text-center text-2xl text-black font-avenirRoman mt-6">
+          <p className="max-w-[600px] text-center text-xl sm:text-2xl text-black font-avenirRoman mt-6">
             we’re sorry. the page you requested could no be found Please go back
             to the home page
           </p>
