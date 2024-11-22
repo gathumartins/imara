c3a8dac14b7ffa7e6779e2573196079bed91318d
gathumartins <gathumartins@gmail.com>
2024-11-22 12:02:32 +0300

    worked on 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import Link from 'next/link';
 
 function NotFound() {
   return (
-    <div>not-found</div>
-  )
+    <article
+      className="bg-cover h-lvh bg-center"
+      style={{ backgroundImage: "url(/images/404.webp)" }}
+    >
+      <section className="container h-full py-12 px-4 flex justify-center place-items-center md:justify-end md:place-items-end">
+        <div className="max-w-[600px] md:w-[600px] flex flex-col">
+          <h1 className="text-iBlue font-avenirBlack text-9xl text-center mb-10">
+            404
+          </h1>
+          <h2 className="text-black font-avenirNextRegular text-6xl text-center">
+            Page Not Found
+          </h2>
+          <p className="max-w-[600px] text-center text-2xl text-black font-avenirRoman mt-6">
+            we’re sorry. the page you requested could no be found Please go back
+            to the home page
+          </p>
+          <Link href="/" className="bg-iBlue my-8 hover:bg-iSecondary text-white w-[180px] mx-auto p-3 text-center rounded text-xl font-avenirRoman">Go Home</Link>
+        </div>
+      </section>
+    </article>
+  );
 }
 
-export default NotFound
+export default NotFound 
