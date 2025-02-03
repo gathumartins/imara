9a79721547b350dd7996817020dda9462cd0c1de
gathumartins <gathumartins@gmail.com>
2025-02-03 14:08:26 +0300

    updated the register component image to be dynamic

--- src/app/components/Register.tsx
+++ src/app/components/Register.tsx
@@ -4,16 +4,19 @@ import Image from "next/image";
 import Link from "next/link";
 
 function Register({lay}:any) {
+  console.log(lay.register.registerImage);
   return (
     <section className="bg-white">
       <div className="container py-12 flex flex-col-reverse md:flex-row justify-between gap-10 place-items-center">
-        <Image
-          src="/images/registerimg.webp"
-          alt="Imara Fellowship resister section image"
-          width={1626}
-          height={840}
-          className="inline-block rounded flex-grow md:w-1/2"
-        />
+        {lay.register.registerImage !== null && (
+          <Image
+            src={lay.register.registerImage.node.sourceUrl}
+            alt={`Imara Fellowship Africa Resgister Section Image`}
+            width={lay.register.registerImage.node.mediaDetails.width}
+            height={lay.register.registerImage.node.mediaDetails.height}
+            className="inline-block rounded flex-grow md:w-1/2"
+          />
+        )}
         <div className="flex flex-col justify-start md:w-1/2 gap-4">
           <h3 className="font-avenirBlack text-3xl text-iBlue lg:w-3/4">
             {lay.register.registerTitle}
