766e8905a48a3586e1238ac13ecf52435fa80a5a
gathumartins <gathumartins@gmail.com>
2025-02-06 08:05:16 +0300

    updated about us component

--- src/app/components/AboutUs.tsx
+++ src/app/components/AboutUs.tsx
@@ -6,7 +6,7 @@ function AboutUs({content, img}:any) {
     <article className="bg-white">
       <section className="container py-12 flex flex-col lg:flex-row lg:justify-between gap-5">
         <div
-          className="lg:w-2/3 [&_h3]:font-avenirBlack [&_h3]:text-3xl [&_h3]:text-iBlue [&_h3]:mb-5 [&_p]:text-base [&_p]:text-iNeutral [&_p]:font-avenir [&_p]:mb-5 myPro"
+          className="lg:w-2/3 [&_h3]:font-avenirBlack [&_h3]:text-3xl [&_h3]:text-iBlue [&_h3]:mb-5 [&_p]:text-base [&_p]:text-iNeutral [&_p]:font-avenir [&_p]:mb-5 myPro overflow-y-scroll max-h-[350px]"
           dangerouslySetInnerHTML={{ __html: content }}
         ></div>
         {img !== null && (
